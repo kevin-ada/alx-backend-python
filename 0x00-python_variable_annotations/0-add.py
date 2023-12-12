0d1d5859a454cce2b67df57e51b7081e32c45366
kim-thuita <kevinkoech265@gmail.com>
2023-12-12 13:56:42 +0300

    "types in python using hints"

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
+"""Task 0 modules"""
 def add(a: float, b: float) -> float:
+    """Returns two adding numbers"""
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
+"""Task 1"""
 def concat(str1: float, str2: float) -> float:
+    """Returns two strring conctatenetaed"""
     return str1 + str2
