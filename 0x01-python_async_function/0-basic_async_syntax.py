568c166561fec7b3c0989e524d525ed7ff06bbb5
kevin-ada <kevinkoech265@gmail.com>
2023-12-12 10:19:15 +0000

    file syntax

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,9 +4,8 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay:int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """Returning a random value after a certain duration of time 1-10"""
     delay = random.random() * max_delay
     await asyncio.sleep(delay)
     return delay
-
